c247b54f1491b6fbd48d9988cb352096fa856a55
yzhou <yzhou@elijah.com.cn>
2015-06-19 15:34:32 +0800

    go to validate

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,67 @@
 Posts = new Mongo.Collection('posts');
+
+Posts.allow({
+  update: function(userId, post) { return ownsDocument(userId, post); },
+  remove: function(userId, post) { return ownsDocument(userId, post); },
+});
+
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // 只能更改如下两个字段：
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
+
+Posts.deny({
+  update: function(userId, post, fieldNames, modifier) {
+    var errors = validatePost(modifier.$set);
+    return errors.title || errors.url;
+  }
+});
+
+Meteor.methods({
+  postInsert: function(postAttributes) {
+    check(Meteor.userId(), String);
+    check(postAttributes, {
+      title: String,
+      url: String
+    });
+
+    var errors = validatePost(postAttributes);
+    if (errors.title || errors.url)
+      throw new Meteor.Error('invalid-post', "你必须为你的帖子填写标题和 URL");
+
+    var postWithSameUrl = Posts.findOne({url:postAttributes.url});
+    if (postWithSameUrl) {
+    	return {
+    		postExist: true,
+    		_id: postWithSameUrl._id
+    	}
+    }
+
+    var user = Meteor.user();
+    var post = _.extend(postAttributes, {
+      userId: user._id, 
+      author: user.username, 
+      submitted: new Date()
+    });
+
+    var postId = Posts.insert(post);
+
+    return {
+      _id: postId
+    };
+  }
+});
+
+validatePost = function (post) {
+  var errors = {};
+
+  if (!post.title)
+    errors.title = "请填写标题";
+
+  if (!post.url)
+    errors.url =  "请填写 URL";
+
+  return errors;
+}
